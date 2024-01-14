53e3dda632a6a0a6c5131d7203d74dba16f67959
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-01-13 23:50:56 -0600

    Can now read/create Tags

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_app.models.Tag import Tag
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
     return render_template('index.jinja2', test='asdf')
 
-@app.route('/test')
-def test():
-    db = SQLAlchemy(model_class=Tag)
-    return db.session.query(Tag).all()
+
+@app.route('/tags', methods=['GET'])
+def all_tags():
+    tags = db.session.query(Tag).all()
+
+    return jsonify([tag.to_dict() for tag in tags])
+
+
+@app.route('/tags', methods=['POST'])
+def make_tag():
+    tag = Tag(name=request.form['name'])
+
+    db.session.add(tag)
+    db.session.commit()
+
+    return "OK"
--- flask_app/models/Tag.py
+++ flask_app/models/Tag.py
@@ -1,4 +1,24 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, String, Integer, Float, Boolean
+from sqlalchemy.orm import declarative_base
 
-class Tag(DeclarativeBase):
-    pass
+Base = declarative_base()
+
+
+class Tag(Base):
+    __tablename__ = 'tags'
+
+    name = Column(String, primary_key=True)
+
+    value_int = Column(Integer, nullable=True)
+    value_float = Column(Float, nullable=True)
+    value_string = Column(String, nullable=True)
+    value_bool = Column(Boolean, nullable=True)
+
+    def to_dict(self) -> dict:
+        return {
+            'name': self.name,
+            'value_int': self.value_int,
+            'value_float': self.value_float,
+            'value_string': self.value_string,
+            'value_bool': self.value_bool
+        }
