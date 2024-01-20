2ce0036f8906a1c3d8632f6a07eff221da72c85d
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-01-20 15:36:17 -0600

    Now have device routes to add a tag to a device

--- flask_app/routes/device.py
+++ flask_app/routes/device.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, current_app
 
 from flask_app.models.Device import Device
+from flask_app.models.Tag import Tag
 
 routes = Blueprint('device_routes', __name__)
 
@@ -26,12 +27,35 @@ def make_tag():
     return jsonify("Success")
 
 
-@routes.route('/device/<id>', methods=['DELETE'])
+@routes.route('/devices/<id>', methods=['DELETE'])
 def delete_tag(id):
     db = current_app.db
     device = db.session.query(Device).filter(Device.id == id).first()
 
     db.session.delete(device)
+    db.session.commit()
+
+    return jsonify("Success")
+
+
+@routes.route('/devices/<device_id>/tags', methods=['GET'])
+def get_device_tags(device_id):
+    db = current_app.db
+    device = db.session.query(Device).filter(Device.id == device_id).first()
+
+    return jsonify([tag.to_dict() for tag in device.tags])
+
+
+@routes.route('/devices/<device_id>/tags', methods=['POST'])
+def add_tag_to_device(device_id):
+    db = current_app.db
+
+    device = db.session.query(Device).filter(Device.id == device_id).first()
+
+    tag = Tag(name=request.form.get('name'), device_id=device_id)
+
+    db.session.add(tag)
+    device.tags.append(tag)
     db.session.commit()
 
     return jsonify("Success")
