21fbfdbadf6548de31952903a78380536c3deb60
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-01-21 14:19:18 -0600

    Moved routes under http folder

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,9 +5,9 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_app.models.Base import Base
-from flask_app.routes.device import routes as device_routes
-from flask_app.routes.modbus import routes as modbus_routes
-from flask_app.routes.tag import routes as tag_routes
+from flask_app.routes.http.device import routes as device_routes
+from flask_app.routes.http.modbus import routes as modbus_routes
+from flask_app.routes.http.tag import routes as tag_routes
 
 load_dotenv()
 
@@ -17,7 +17,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 with app.app_context():
     db = SQLAlchemy(model_class=Base)
     db.init_app(app)
-
     app.db = db
 
 app.register_blueprint(tag_routes)
