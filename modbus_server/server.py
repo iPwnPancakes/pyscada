b0c24c457d1ef6b2be161475c8b560b1369342cd
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-01-21 13:37:14 -0600

    Reformatted file

--- modbus_server/server.py
+++ modbus_server/server.py
@@ -2,10 +2,10 @@ from pymodbus.server import StartTcpServer
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext, ModbusSequentialDataBlock
 
 store = ModbusSlaveContext(
-    di = ModbusSequentialDataBlock(0, [0]*100),
-    co = ModbusSequentialDataBlock(0, [0]*100),
-    hr = ModbusSequentialDataBlock(0, [1]*100),
-    ir = ModbusSequentialDataBlock(0, [0]*100)
+    di=ModbusSequentialDataBlock(0, [0] * 100),
+    co=ModbusSequentialDataBlock(0, [0] * 100),
+    hr=ModbusSequentialDataBlock(0, [1] * 100),
+    ir=ModbusSequentialDataBlock(0, [0] * 100)
 )
 
 context = ModbusServerContext(slaves=store, single=True)
