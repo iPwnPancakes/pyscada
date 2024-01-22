b40c1b1fb158c71070416bc221a2bc879269f85a
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-01-21 19:42:08 -0600

    Now using application factory pattern to create app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,37 +1,10 @@
-import os
-
 from dotenv import load_dotenv
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-from flask_app.lib.mqtt.Client import Client
-from flask_app.models.Base import Base
-from flask_app.routes.http.device import routes as device_routes
-from flask_app.routes.http.modbus import routes as modbus_routes
-from flask_app.routes.http.tag import routes as tag_routes
-from flask_app.routes.http.ui import routes as ui_routes
-from flask_app.routes.mqtt.device import routes as device_mqtt_routes
+from flask_app.factory import create_flask_app, create_mqtt_app
 
 load_dotenv()
 
-# Flask HTTP setup
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app = create_flask_app()  # Automatically ran when using `flask run` command
+mqtt = create_mqtt_app()
 
-with app.app_context():
-    db = SQLAlchemy(model_class=Base)
-    db.init_app(app)
-    app.db = db
-
-app.register_blueprint(ui_routes)
-app.register_blueprint(tag_routes)
-app.register_blueprint(device_routes)
-app.register_blueprint(modbus_routes)
-
-# MQTT client setup
-mqtt_client = Client(
-    host=os.environ['MQTT_HOST'],
-    port=int(os.environ['MQTT_PORT']),
-)
-mqtt_client.register_routes(device_mqtt_routes)
-mqtt_client.run()
+mqtt.run()
---
+++ flask_app/factory.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_app.lib.mqtt.Client import Client
+from flask_app.models.Base import Base
+from flask_app.routes.http.device import routes as device_routes
+from flask_app.routes.http.modbus import routes as modbus_routes
+from flask_app.routes.http.tag import routes as tag_routes
+from flask_app.routes.http.ui import routes as ui_routes
+from flask_app.routes.mqtt.device import routes as device_mqtt_routes
+
+
+def create_mqtt_app() -> Client:
+    mqtt_client = Client(
+        host=os.environ['MQTT_HOST'],
+        port=int(os.environ['MQTT_PORT']),
+    )
+
+    mqtt_client.register_routes(device_mqtt_routes)
+
+    return mqtt_client
+
+
+def create_flask_app() -> Flask:
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+
+    with app.app_context():
+        db = SQLAlchemy(model_class=Base)
+        db.init_app(app)
+        app.db = db
+
+    app.register_blueprint(ui_routes)
+    app.register_blueprint(tag_routes)
+    app.register_blueprint(device_routes)
+    app.register_blueprint(modbus_routes)
+
+    return app
