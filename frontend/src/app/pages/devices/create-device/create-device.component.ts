b1597d56d193c23bad167d535c8283c79f4f46f5
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-02-02 17:59:39 -0600

    Created first form

--- frontend/src/app/pages/devices/create-device/create-device.component.ts
+++ frontend/src/app/pages/devices/create-device/create-device.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-create-device',
     standalone: true,
-    imports: [],
+    imports: [MatFormField, MatLabel, MatInput, MatButton, ReactiveFormsModule],
     templateUrl: './create-device.component.html',
     styleUrl: './create-device.component.scss'
 })
 export class CreateDeviceComponent {
+    createDeviceForm: FormGroup = new FormGroup({
+        name: new FormControl('')
+    });
 
+    public onSubmit(): void {
+        console.log(this.createDeviceForm);
+    }
 }
