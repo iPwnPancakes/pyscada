6493892ae3118adc1eac928891022667af93fa0c
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-02-04 18:24:15 -0600

    Removed hello world file

--- start_timer_process.py
+++ start_timer_process.py
@@ -2,13 +2,11 @@ from dotenv import load_dotenv
 
 from timer_process.Process import Process
 from timer_process.jobs.PollTagModbusValues import PollTagModbusValues
-from timer_process.jobs.hello import HelloWorld
 
 load_dotenv()
 
 process = Process()
 
-process.register_job(HelloWorld('* * * * * *'))
 process.register_job(PollTagModbusValues('* * * * * *'))
 
 process.run()
